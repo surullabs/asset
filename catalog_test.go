754a087ced7a098ec4cc84b91109b47346ad5954
sridharv <sridhar@laddoo.net>
2016-09-19 15:25:44 +0200

    Move to testify/require

--- catalog_test.go
+++ catalog_test.go
@@ -7,41 +7,36 @@ import (
 	"io/ioutil"
 	"os"
 
-	"bytes"
-
-	"github.com/sridharv/fail"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCatalog(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
-
-	defer fail.Using(t.Fatal)
-
 	tmpDir, err := ioutil.TempDir("", "catalog-test")
-	fail.IfErr(err)
+	require.NoError(t, err)
 	defer os.RemoveAll(tmpDir)
 
 	catalogDir := filepath.Join(tmpDir, "TestCatalog.xcassets")
-	fail.IfErr(os.MkdirAll(catalogDir, 0700))
+	require.NoError(t, os.MkdirAll(catalogDir, 0700))
 	catalog, err := NewCatalog(catalogDir)
-	fail.IfErr(err)
+	require.NoError(t, err)
 
 	// Test data sourced from https://github.com/encharm/Font-Awesome-SVG-PNG
 	err = catalog.AddSVGs("testdata/data")
 	if err == ErrNoInkScape {
 		t.Skip(err.Error())
 	}
-	fail.IfErr(err)
-	fail.IfErr(catalog.Write())
+	require.NoError(t, err)
+	require.NoError(t, catalog.Write())
 
-	golden := listAll("testdata/TestCatalog.xcassets")
-	actual := listAll(catalogDir)
-	fail.If(len(golden) != len(actual), "expected ", len(golden), " files, got ", len(actual))
+	golden := listAll(t, "testdata/TestCatalog.xcassets")
+	actual := listAll(t, catalogDir)
+	require.Equal(t, len(golden), len(actual), "expected ", len(golden), " files, got ", len(actual))
 	for i, g := range golden {
-		fail.If(g.name != actual[i].name, "expected ", g.name, ", got ", actual[i].name)
-		fail.If(!bytes.Equal(g.contents, actual[i].contents), "contents of ", g.name, " not equal")
+		require.Equal(t, g.name, actual[i].name, "expected ", g.name, ", got ", actual[i].name)
+		require.Equal(t, g.contents, actual[i].contents, "contents of ", g.name, " not equal")
 	}
 }
 
@@ -50,9 +45,9 @@ type catalogFile struct {
 	contents []byte
 }
 
-func listAll(dir string) []catalogFile {
+func listAll(t *testing.T, dir string) []catalogFile {
 	files := []catalogFile{}
-	fail.IfErr(filepath.Walk(dir, func(path string, stat os.FileInfo, err error) error {
+	require.NoError(t, filepath.Walk(dir, func(path string, stat os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
