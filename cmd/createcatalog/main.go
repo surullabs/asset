5ef8f3cb1ae7e9fd94bb0606e76be388f9dffd00
sridharv <sridhar@laddoo.net>
2016-09-19 18:41:52 +0200

    Command line tool for asset generation

---
+++ cmd/createcatalog/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/surullabs/asset"
+)
+
+func validate(out string) error {
+	if out == "" {
+		return errors.New("no output directory specifed")
+	}
+	if len(flag.Args()) != 1 {
+		return errors.New("no input directory specified")
+	}
+	if filepath.Ext(out) != ".xcassets" {
+		return fmt.Errorf("unsupported output directory %s (must be end in .xcassets)", out)
+	}
+	return nil
+}
+
+func gen(out string, force bool) error {
+	c, err := asset.NewCatalog(out)
+	if err != nil {
+		return err
+	}
+	converter, err := asset.StartPhantomJSConverter()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := converter.Stop(); err != nil {
+			fmt.Fprintln(os.Stderr, "WARNING: failed to stop phantomjs cleanly: %v", err)
+		}
+	}()
+	if err := c.AddSVGs(flag.Args()[0], force, converter); err != nil {
+		return err
+	}
+	return c.Write()
+}
+
+func main() {
+	var out string
+	var force bool
+	flag.StringVar(&out, "out", "", "Output directory for the asset catalog")
+	flag.BoolVar(&force, "force", false, "If true all svgs are updated")
+	flag.Parse()
+
+	if err := validate(out); err != nil {
+		fmt.Fprintf(os.Stderr, "Usage: %s --out <path/to/Catalog.xcassets> <src>\n", os.Args[0])
+		flag.Usage()
+		os.Exit(1)
+		return
+	}
+
+	err := gen(out, force)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
